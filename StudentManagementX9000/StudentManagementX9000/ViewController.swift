83a2e5df06f9cf6159f5c6b1dce9683c78f521a5
Bui Quang Tri <bqt1816@gmail.com>
2020-04-14 21:24:26 +0700

    add project and pod file

---
+++ StudentManagementX9000/StudentManagementX9000/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudentManagementX9000
+//
+//  Created by Bui Quang Tri on 4/14/20.
+//  Copyright © 2020 Bui Quang Tri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
